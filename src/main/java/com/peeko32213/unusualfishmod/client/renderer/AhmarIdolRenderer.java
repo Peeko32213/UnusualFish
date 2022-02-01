02f6b8cb6c5681c19e8480727f6fb066bc3d12f6
HugoMage <69723114+HugoMage@users.noreply.github.com>
2022-01-31 19:07:49 -0600

    Update AhmarIdolRenderer.java

--- src/main/java/com/peeko32213/unusualfishmod/client/renderer/AhmarIdolRenderer.java
+++ src/main/java/com/peeko32213/unusualfishmod/client/renderer/AhmarIdolRenderer.java
@@ -20,7 +20,17 @@ public AhmarIdolRenderer(Context context) {
 super(context, new AhmarIdolModel<>(context.bakeLayer(AhmarIdolModel.LAYER_LOCATION)), 0.4f);
 
 }
+//this code right here is what you insert when you want to give side flop to your fish \/
+	 protected void setupRotations(AhmarIdolEntity p_116226_, PoseStack p_116227_, float p_116228_, float p_116229_, float p_116230_) {
+      super.setupRotations(p_116226_, p_116227_, p_116228_, p_116229_, p_116230_);
+      float f = 4.3F * Mth.sin(0.6F * p_116228_);
+      p_116227_.mulPose(Vector3f.YP.rotationDegrees(f));
+      if (!p_116226_.isInWater()) {
+         p_116227_.translate((double)0.2F, (double)0.1F, 0.0D);
+         p_116227_.mulPose(Vector3f.ZP.rotationDegrees(90.0F));
+      }
 
+   }
 @Override
 public ResourceLocation getTextureLocation(Type entity) {
 

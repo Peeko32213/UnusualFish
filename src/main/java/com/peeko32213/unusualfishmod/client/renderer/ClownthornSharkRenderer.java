7da7f7ae62b5d67bffaf90656c2bf2bed904f909
HugoMage <69723114+HugoMage@users.noreply.github.com>
2022-01-31 19:09:01 -0600

    Update ClownthornSharkRenderer.java

--- src/main/java/com/peeko32213/unusualfishmod/client/renderer/ClownthornSharkRenderer.java
+++ src/main/java/com/peeko32213/unusualfishmod/client/renderer/ClownthornSharkRenderer.java
@@ -15,7 +15,17 @@ extends MobRenderer<Type, ClownthornSharkModel<Type>> {
 
 private static final ResourceLocation TEXTURE = new ResourceLocation(UnusualFishMod.MODID,
 	"textures/entities/clownthornshark.png");
+//this code right here is what you insert when you want to give side flop to your fish \/
+	 protected void setupRotations(ClownthornSharkEntity p_116226_, PoseStack p_116227_, float p_116228_, float p_116229_, float p_116230_) {
+      super.setupRotations(p_116226_, p_116227_, p_116228_, p_116229_, p_116230_);
+      float f = 4.3F * Mth.sin(0.6F * p_116228_);
+      p_116227_.mulPose(Vector3f.YP.rotationDegrees(f));
+      if (!p_116226_.isInWater()) {
+         p_116227_.translate((double)0.2F, (double)0.1F, 0.0D);
+         p_116227_.mulPose(Vector3f.ZP.rotationDegrees(90.0F));
+      }
 
+   }
 public ClownthornSharkRenderer(Context context) {
 super(context, new ClownthornSharkModel<>(context.bakeLayer(ClownthornSharkModel.LAYER_LOCATION)), 0.4f);
 
